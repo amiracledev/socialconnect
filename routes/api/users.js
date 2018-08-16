8d9c0d884f41f7d0eccd84c6965450e6b67490d7
Amir Nickroo <amiracledev@gmail.com>
2018-08-16 16:33:16 -0700

    set up user model and route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+//Load User Model
+const User = require("../../models/User");
+
 //@routes   GET api/users/test
 //@desc     Tests Users route
 //@access   Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+//@routes   GET api/users/register
+//@desc     Register user
+//@access   Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exist" });
+    }
+  });
+});
 
 module.exports = router;
