f76e348dfc3ba8b2ac4d740d9229eb05fa99817a
Amir Nickroo <amiracledev@gmail.com>
2018-09-01 08:28:53 -0700

    connecting Redux

---
+++ client/src/reducers/errorReducer.js
@@ -0,0 +1,15 @@
+import { GET_ERRORS } from "../actions/types";
+
+const initialState = {
+  isAuthenticated: false,
+  user: {
+    user: {}
+  }
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
