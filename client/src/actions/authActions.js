298c6543ca3471b186dbb59355afdb1d81090c76
Amir Nickroo <amiracledev@gmail.com>
2018-09-05 10:34:35 -0700

    login redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 //Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -13,4 +15,35 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//Login -
+export const loginUser = userData => disp => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //save locally
+      const { token } = res.data;
+      //set to ls
+      localStorage.setItem("jwtToken", token);
+      //Set token to Auth header
+      setAuthToken(token);
+      //Decode token to grab user info
+      const decoded = jwt_decode(token);
+      //Set User
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+//Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
